373c50c52a6235e523d7cfa05c82d9168b4a3c59
systematicRealm <119763173+systematicRealm@users.noreply.github.com>
2022-12-05 21:42:12 +0300

    finished why us

--- src/components/02_body/components/02-whyUs/WhyUs.js
+++ src/components/02_body/components/02-whyUs/WhyUs.js
@@ -1,17 +1,31 @@
 import React from "react";
 import CardWhyUs from "./components/CardWhyUs";
 import assure from "./assets/assurance.svg";
+import construct from "./assets/construction.svg";
+import delivery from "./assets/onTime.svg";
+import plan from "./assets/planning.svg";
+import hq from "./assets/quality.svg";
+import support from "./assets/support.svg";
+import volunteer from "./assets/volunteer.svg";
 
 export default function WhyUs() {
+  const points = [
+    "On-time Completion",
+    "Quality Assurance",
+    "Competitive Prices",
+    "Technical Support",
+    "Accurate Planning",
+    "Safety Management",
+  ];
   return (
     <div className="flex items-center justify-center w-full bg-neutral-500 h-100v">
       <div className="flex items-center justify-center w-3/4 gap-2">
-        <CardWhyUs icon={assure} text={"Assurence Lalaity"} />
-        <CardWhyUs icon={assure} text={"Assurence Lalaity"} />
-        <CardWhyUs icon={assure} text={"Assurence Lalaity"} />
-        <CardWhyUs icon={assure} text={"Assurence Lalaity"} />
-        <CardWhyUs icon={assure} text={"Assurence Lalaity"} />
-        <CardWhyUs icon={assure} text={"Assurence Lalaity"} />
+        <CardWhyUs icon={delivery} text={points[0]} />
+        <CardWhyUs icon={assure} text={points[1]} />
+        <CardWhyUs icon={volunteer} text={points[2]} />
+        <CardWhyUs icon={support} text={points[3]} />
+        <CardWhyUs icon={plan} text={points[4]} />
+        <CardWhyUs icon={hq} text={points[5]} />
       </div>
     </div>
   );
