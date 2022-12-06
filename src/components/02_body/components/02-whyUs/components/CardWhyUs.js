a38de5343a4ce918ce1d608395cd140e72de4445
Ahmed Rowaihi <ahmedrowaihi1@gmail.com>
2022-12-06 15:32:25 -0800

    easier point structure and mapping.

--- src/components/02_body/components/02-whyUs/components/CardWhyUs.js
+++ src/components/02_body/components/02-whyUs/components/CardWhyUs.js
@@ -1,13 +1,15 @@
 import React from "react";
-import '../WhyUs.css'
+import "../WhyUs.css";
 
 export default function CardWhyUs({ icon, text }) {
   return (
     <div className="flex flex-col justify-center flex-1 overflow-hidden bg-white rounded-lg shadow">
       <span className="flex items-center justify-center w-full py-3 bg-theme-red">
-        <img id="WhyUs-icon" src={icon} />
+        <img id="WhyUs-icon" src={icon} alt={text} />
       </span>
-      <span className="flex items-center justify-center h-full py-12 font-medium">{text}</span>
+      <span className="flex items-center justify-center h-full py-12 font-medium">
+        {text}
+      </span>
     </div>
   );
 }
