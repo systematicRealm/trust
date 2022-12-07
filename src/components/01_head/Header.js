58bc689bc1a2f44909a9e37f3ae75a890ced356b
systematicRealm <119763173+systematicRealm@users.noreply.github.com>
2022-12-07 09:30:56 +0300

    end of morning session

--- src/components/01_head/Header.js
+++ src/components/01_head/Header.js
@@ -27,17 +27,25 @@ export default function Header() {
     };
   }, []);
 
+  const NavElementsStyles =
+    "px-2 py-2 transition-colors duration-300 cursor-pointer hover:bg-neutral-200 rounded-3xl hover:text-black max-sm:text-sm";
+
   return (
     <div
       ref={header}
-      className="absolute top-0 z-40 flex items-center justify-center w-full py-2 text-white transition-all duration-500"
+      className="absolute top-0 z-40 flex items-center justify-center w-full py-2 transition-all duration-500 text-neutral-200"
     >
       <div className="flex items-center justify-between w-3/4 max-sm:w-full max-sm:justify-around">
         <div
           onClick={scrollToTop}
           className="flex items-center justify-center w-10 transition-colors duration-300 rounded-full cursor-pointer"
         >
-          <img className="w-full max-sm:w-9" src={logo} alt="trust logo" id="logo" />
+          <img
+            className="w-full max-sm:w-9"
+            src={logo}
+            alt="trust logo"
+            id="logo"
+          />
         </div>
 
         <div className="flex items-center justify-center gap-5 font-normal max-sm:gap-2">
@@ -49,7 +57,11 @@ export default function Header() {
           ].map((item, index) => (
             <span key={index}>
               <a
-                className="px-2 py-2 transition-colors duration-300 cursor-pointer hover:bg-neutral-200 rounded-3xl hover:text-black max-sm:text-sm"
+                className={
+                  index !== 3
+                    ? NavElementsStyles
+                    : NavElementsStyles + " text-white"
+                }
                 href={item.href}
               >
                 {item.name}
